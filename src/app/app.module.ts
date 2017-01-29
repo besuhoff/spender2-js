d2cdcf3d4ea50b4613d3e8537f7a15aaff0eb41e
Albert Besuhoff <albert.besuhoff@gmail.com>
2017-01-29 20:47:14 +0200

    Upgrade cli and packages versions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -79,7 +79,6 @@ import { LoginFormComponent } from './login-form/login-form.component';
     LaddaModule.forRoot({
       style: 'slide-right',
       spinnerColor: '#2D2B29'
-
     }),
     NgbModule.forRoot(),
     ChartsModule,
@@ -95,9 +94,7 @@ import { LoginFormComponent } from './login-form/login-form.component';
     HttpClientService,
     {
       provide: HttpClientService,
-      useFactory: (backend: XHRBackend, options: RequestOptions, gapiService: GapiService) => {
-        return new HttpClientService(backend, options, gapiService);
-      },
+      useFactory: httpFactory,
       deps: [XHRBackend, RequestOptions, GapiService]
     },
     IncomeService,
@@ -117,3 +114,8 @@ import { LoginFormComponent } from './login-form/login-form.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function httpFactory(backend: XHRBackend, options: RequestOptions, gapiService: GapiService) {
+  return new HttpClientService(backend, options, gapiService);
+}
+
